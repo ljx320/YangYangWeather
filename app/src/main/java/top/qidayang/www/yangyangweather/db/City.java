0104321fac8c8e950c6009b74e23479258d5a912
ljx320 <691330927@qq.com>
2017-01-01 16:50:26 +0800

    add jar and java class

---
+++ app/src/main/java/top/qidayang/www/yangyangweather/db/City.java
@@ -0,0 +1,55 @@
+package top.qidayang.www.yangyangweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 69133 on 2017/1/1.
+ */
+
+public class City extends DataSupport {
+    private  int id;
+    private  String cityName;
+    private int cityCode;
+    private int provinceCode;
+
+
+    public int getId(){
+        return  id;
+
+    }
+
+    public  void setId(int id){
+        this.id=id;
+
+    }
+
+    public String getCityName(){
+        return  cityName;
+
+    }
+
+    public void setCityName(String cityName){
+
+        this.cityName=cityName;
+    }
+
+    public  int getCityCode(){
+        return  this.cityCode;
+
+    }
+
+    public  void  setCityCode(int cityCode){
+        this.cityCode=cityCode;
+
+    }
+
+    public  int getProvinceCode(){
+        return  provinceCode;
+
+    }
+
+    public  void  setProvinceCode(int provinceCode){
+        this.provinceCode=provinceCode;
+
+    }
+}
---
+++ app/src/main/java/top/qidayang/www/yangyangweather/db/Province.java
@@ -0,0 +1,44 @@
+package top.qidayang.www.yangyangweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 69133 on 2017/1/1.
+ */
+
+
+public class Province  extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId(){
+
+        return id;
+    }
+    public  void  setId(int id){
+        this.id=id;
+
+    }
+
+    public String getProvinceName(){
+        return  provinceName;
+
+    }
+
+    public  void setProvinceName(String provinceName){
+        this.provinceName=provinceName;
+
+    }
+
+    public  int getProvinceCode(){
+        return  provinceCode;
+
+    }
+
+    public  void setProvinceCode(int provinceCode){
+        this.provinceCode=provinceCode;
+
+    }
+
+}
